9ed0a90439d38db3438df599fb764fd0e3fe0570
Salfi Sabit <70028517+Salf1-Sabit@users.noreply.github.com>
2023-12-18 18:47:02 +0600

    Create 48. Rotate Image.cpp

---
+++ Leetcode Solutions/48. Rotate Image.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = (int) matrix.size();
+        for (int i = 0, m = n; i < n / 2; ++i, --m) {
+            for (int j = i; j < m - 1; ++j) {
+                int& a = matrix[i][j];
+                int& b = matrix[j][n - 1 - i];
+                int& c = matrix[n - 1 - i][n - 1 - j];
+                int& d = matrix[n - 1 - j][i];
+                int tmp = d;
+                d = c;
+                c = b;
+                b = a;
+                a = tmp;
+            }
+        }
+    }
+};
